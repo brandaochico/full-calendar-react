b7315dcf9fad874335cab10cd14d095cb1c6e0b3
brandaochico <brandao.chico24@gmail.com>
2026-05-27 13:43:39 -0300

    feat: passing colors based on employee id

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,17 +10,40 @@ class HomeController extends Controller
 {
     public function __invoke(): Response
     {
-        $events = Appointment::with(['client', 'employee'])
-            ->get()
-            ->map(fn (Appointment $appointment) => [
-                'title' => $appointment->client->name.' ('.$appointment->employee->name.')',
+        $appointments = Appointment::with(['client', 'employee'])->get();
+
+        $employees = $appointments->pluck('employee')
+            ->filter()
+            ->unique('id')
+            ->sortBy('id')
+            ->values();
+
+        $colors = [
+            '#6A041D',
+            '#F5B841',
+            '#F4FF52',
+            '#53FF45',
+            '#1E2EDE',
+        ];
+
+        $employeesColors = [];
+        foreach ($employees as $index => $employee) {
+            $colorIndex = $index % count($colors);
+            $employeesColors[$employee->id] = $colors[$colorIndex];
+        }
+
+        $events = $appointments->map(function ($appointment) use ($employeesColors) {
+            $employeeId = $appointment->employee?->id;
+
+            return [
+                'title' => $appointment->client->name . ' (' . $appointment->employee->name . ')',
+                'employee' => $appointment->employee?->name,
                 'start' => $appointment->start_time,
                 'end' => $appointment->finish_time,
-            ])
-            ->all();
+                'backgroundColor' => $employeesColors[$employeeId] ?? null,
+            ];
+        })->values()->all();
 
-        return Inertia::render('Home', [
-            'events' => $events,
-        ]);
+        return Inertia::render('Home', compact('events'));
     }
 }
